3772c606053c6241e6775f659c212cccddfc019b
Gaspar Morales <gasparmorales1@gmail.com>
2023-06-10 05:23:12 -0300

    print csv y error con hashmap long e int

--- src/main.java
+++ src/main.java
@@ -1,6 +1,20 @@
+import Entities.User;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import uy.edu.um.prog2.adt.MyHash.HashTableCerradoImpl;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.Scanner;
 
+import static java.lang.Long.parseLong;
+
+
 public class main {
+    static Long idTweet = Long.valueOf(0);
+    static Long idHastag = Long.valueOf(0);
+    static HashTableCerradoImpl<Long, User> listaUsuariosRegistrados = new HashTableCerradoImpl<>(10);
     public static void top_drivers(){
 
     }
@@ -25,7 +39,49 @@ public class main {
     }
 
     public static void load_csv(){
+        try {
+            Reader in = new FileReader("f1_dataset_test.csv");
+            Iterable<CSVRecord> records = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(in);
+            for (CSVRecord record : records) {
+                String id = record.get("");
+                String user_name = record.get("user_name");
+                String user_location = record.get("user_location");
+                String user_description = record.get("user_description");
+                String user_created = record.get("user_created");
+                String user_followers = record.get("user_followers");
+                String user_friends = record.get("user_friends");
+                String user_favourites = record.get("user_favourites");
+                String user_verified = record.get("user_verified");
+                String date = record.get("date");
+                String text = record.get("text");
+                String hashtags = record.get("hashtags");
+                String source = record.get("source");
+                String is_retweet = record.get("is_retweet");
 
+                System.out.println("id " + id);
+                System.out.println("nombre " + user_name);
+                System.out.println(user_location);
+                System.out.println(user_description);
+                System.out.println(user_created);
+                System.out.println(user_followers);
+                System.out.println(user_friends);
+                System.out.println(user_favourites);
+                System.out.println(user_verified);
+                System.out.println(text);
+                System.out.println(hashtags);
+                System.out.println(source);
+                System.out.println(is_retweet);
+                System.out.println("-------------------------");
+
+                if (id != null){
+                    User newUser = new User(parseLong(id), user_name);
+                    listaUsuariosRegistrados.insert(parseLong(id), newUser);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public static void main(String[] args) throws InterruptedException {
         Scanner entrada = new Scanner(System.in);
